3e321a9cc28e97ff2d5f86306622e49f4c24861d
brandonamo <39618557+brandonamo@users.noreply.github.com>
2018-08-15 13:26:58 +1200

    base game

---
+++ StarFishGame/Assets/Scripts/CylinderMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1000.0f;
+        transform.Rotate(0, x, 0);
+    }
+}
---
+++ StarFishGame/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 50.0f;
+        transform.Translate(x, 0, 0);
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 200.0f;
+
+        if (y > 0)
+        {
+            transform.Translate(0, y, 0);
+        }
+    }
+}
