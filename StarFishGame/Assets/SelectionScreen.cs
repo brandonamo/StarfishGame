7df8f448cefd8a8d47810c43f4b27db575460fc8
EthanTheHuman <32722553+EthanTheHuman@users.noreply.github.com>
2018-08-22 16:17:34 +1200

    Added Main Menu stuff

---
+++ StarFishGame/Assets/SelectionScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionScreen : MonoBehaviour {
+
+    private int Selection = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Selection >= 2)
+        {
+            // Do nothing
+        }
+	}
+
+    void AddOne()
+    {
+        Selection++;
+    }
+}
